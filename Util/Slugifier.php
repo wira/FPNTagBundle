ed3f10b574720faccc46d714d937fdd99b8abf74
David Bonachera <david.bonachera@gmail.com>
2017-07-20 16:30:17 +0800

    From @HombreAlmohada https://github.com/HombreAlmohada/FPNTagBundle/commit/fd1e0c203ade8aeb57b429222ae6cce7c6a27bdd

--- Util/Slugifier.php
+++ Util/Slugifier.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the FPNTagBundle package.
  * (c) 2011 Fabien Pennequin <fabien@pennequin.me>
@@ -7,9 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace FPN\TagBundle\Util;
-
 class Slugifier implements SlugifierInterface
 {
     public function slugify($name)
@@ -17,7 +14,17 @@ class Slugifier implements SlugifierInterface
         $slug = mb_convert_case($name, MB_CASE_LOWER, mb_detect_encoding($name));
         $slug = str_replace(' ', '-', $slug);
         $slug = str_replace('--', '-', $slug);
-
+        //Remove accents and spanish letter ñ
+        $slug = $this->normalize($slug);
         return $slug;
+    }
+
+    private function normalize ($cadena){
+        $originales = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿŔŕÑñ';
+        $modificadas ='aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyrrnn';
+        $cadena = utf8_decode($cadena);
+        $cadena = strtr($cadena, utf8_decode($originales), $modificadas);
+        $cadena = strtolower($cadena);
+        return utf8_encode($cadena);
     }
 }
